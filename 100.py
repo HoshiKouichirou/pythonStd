a52aed6832939c66d19ed7603c32e8f35b9a2d16
poyo555 <kou555.hoshi@gmail.com>
2019-10-29 13:12:44 +0900

    100本ノック

---
+++ 100.py
@@ -0,0 +1,21 @@
+# text = "stressed"
+# ans = ""
+# for i in range(len(text)):
+#   ans =text[i]+ans
+# else:
+#   print(ans)
+# text1 = "パトカー"
+# text2 = "タクシー"
+# ans = ""
+# for i in range(len(text1)):
+#   ans+=text1[i]+text2[i]
+# else:
+#   print(ans)
+# text = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+# text = text.replace(".", "")
+# text = text.split(" ")
+# ans = []
+# for val in text:
+#   ans.append(len(val))
+# else:
+#   print(ans)
